b0e9f511e742613ec687083e5d85c418922282f2
91am <killaek.info@gmail.com>
2023-11-12 12:43:43 +0100

    Test functions

--- 03-functions.test.js
+++ 03-functions.test.js
@@ -8,8 +8,9 @@ describe("Funciones en JavaScript", () => {
     it("Las funciones pueden tener 1 parámetro", () => {
         let name = "Rosa";
 
-        // Escribe aquí tu funcion "saludar"
-
+        function saludar(name){
+         return `Hola, ${name}`;
+        }
 
         assert.strictEqual(saludar(name),"Hola, Rosa");
     })
@@ -19,8 +20,9 @@ describe("Funciones en JavaScript", () => {
         let num1 = 17;
         let num2 = 10;
 
-        // Escribe aquí tu función "suma"
-
+        function suma(num1, num2){
+            return num1 + num2
+        }
 
         assert.strictEqual(suma(num1, num2),27);
     })
@@ -33,9 +35,13 @@ describe("Funciones en JavaScript", () => {
         let weather = "soleado";
         let rainProbability = "10%";
 
+        function weatherReport(userName, name, city, weather, rainProbability){
+            return `Hola ${name} (${userName}), hoy en ${city} el tiempo es ${weather} y las probabilidades de lluvia son del ${rainProbability}.`
+        }
+
         // Escribe aquí tu función "weatherReport" (fíjate en la aserción para saber qué debe retornar esta función).
 
-        assert.strictEqual(weatherReport(userName, name, city, weather, rainProbability)).toEqual("Hola Ada Martín (ada89), hoy en Las Vegas el tiempo es soleado y las probabilidades de lluvia son del 10%.");
+        assert.strictEqual(weatherReport(userName, name, city, weather, rainProbability),"Hola Ada Martín (ada89), hoy en Las Vegas el tiempo es soleado y las probabilidades de lluvia son del 10%.")
     })
 
 
